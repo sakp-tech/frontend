7d4c9c159a01a7e865fa929723c4b7270665e633
sakp-tech <61321907+sakp-tech@users.noreply.github.com>
2023-06-20 20:57:49 +0530

    1 file modified

--- Algorithms/Graph/BFS.js
+++ Algorithms/Graph/BFS.js
@@ -10,3 +10,31 @@
  * Queue: To check the nodes in specific order(FIFO).
  * Hash Table: To maintain relation between nodes
  */
+
+function checkIfSeller() {
+  const graph = {};
+  graph['alice'] = ['bob', 'aman', 'rahul'];
+  graph['bob'] = ['rahul', 'peggy'];
+  graph['rahul'] = ['wanda', 'peggy'];
+  graph['wanda'] = ['mango'];
+
+  const queue = graph['alice'];
+
+  const processed = [];
+
+  while (queue.length) {
+    const person = queue.shift();
+    if (processed.indexOf(person) < 0) {
+      console.log('person is', person);
+      if (person === 'mango') {
+        console.log('passed');
+        return true;
+      } else {
+        processed.push(person);
+        graph[person]?.length && queue.push(...graph[person]);
+      }
+    }
+  }
+  console.log('failed');
+  return false;
+}
