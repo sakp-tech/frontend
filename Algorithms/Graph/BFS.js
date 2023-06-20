f7833941469d689233431b88c35485667f80c89d
sakp-tech <61321907+sakp-tech@users.noreply.github.com>
2023-06-20 19:34:38 +0530

    1 file modified

--- Algorithms/Graph/BFS.js
+++ Algorithms/Graph/BFS.js
@@ -1,1 +1,12 @@
+// BFS - Def
+/**
+ * BFS or Breath First Search is an Algorithm which is
+ * used to find if there is a path and if there is then what is the shorted path in unweighted graph.
+ */
 
+// BFS - DS
+/**
+ * To implement BFS we need two Data Structures: Queue and Hash Table
+ * Queue: To check the nodes in specific order(FIFO).
+ * Hash Table: To maintain relation between nodes
+ */
